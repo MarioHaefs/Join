5266d194a869743ea823b772ab6ab82f59f6a1fd
JohannesSpringer <johannes.springer.01@gmx.de>
2023-04-08 17:39:35 +0200

    change: single task in board

--- js/board.js
+++ js/board.js
@@ -7,12 +7,36 @@ function renderTasks() {
     renderSingleTask();
 }
 
+/**
+ * render card for single task
+ * @param {string} taskStatus - Status / column of the task
+ * @todo complete task
+ */
 function renderSingleTask() {
     let todo = document.getElementById('tasks-to-do');
     todo.innerHTML = '';
     todo.innerHTML += `
         <div class="task" id="task1">
-            test1
+            ${htmlTaskTopic()}
+            ${htmlTaskTitle()}
         </div>
+    `;
+}
+
+/**
+ * 
+ * @returns html code for the topic of the task
+ */
+function htmlTaskTopic() {
+    return `
+        <div class="task-topic">
+            Topic
+        </div>
+    `;
+}
+
+function htmlTaskTitle() {
+    return `
+        <h4>Titel</h4>
     `;
 }
